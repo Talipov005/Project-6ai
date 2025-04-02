f4307cb80f4237047ce55b236c28251ba554bf43
Tilekf3 <tilekulanovich@gmail.com>
2025-04-02 16:11:22 +0600

    added header

--- project-6ai/src/components/Header/Header.jsx
+++ project-6ai/src/components/Header/Header.jsx
@@ -9,50 +9,98 @@ import cart from "../../assets/svg/cart.svg"
 
 function Header() {
   return (
-    <header>
-      {/* Верхний блок */}
-      <div className="header-top wrapper">
-        <div className="nur container">
-          <div className="header-top-left">
-          <ul className="header-top-left">
-            <li>О компании</li>
-            <li>Доставка и оплата</li>
-            <li>Гарантии</li>
-            <li>Контакты</li>
-          </ul>
-          </div>
-          <div className="header-top-right">
-            <div className="til">
-              <img src={call} alt="Иконка телефона" />
-              <h4>+7 (965) 237-44-49</h4>
+    <header className="header">
+      <div className="all-header wrapper">
+        <div className="top-header  ">
+            <div className="top-container container">
+            <div className="top-header-left ">
+              <ul>
+                <li>О компании</li>
+                <li>Доставка и оплата</li>
+                <li>Гарантии</li>
+                <li>Контакты</li>
+              </ul>
             </div>
-            <select name="language">
-              <option value="ru">Русский</option>
-              <option value="kg">Кыргызча</option>
-              <option value="en">English</option>
-            </select>
-            <p>Личный кабинет</p>
-          </div>
+            <div className="top-header-right ">
+              <div className="contact">
+                <img src={call} alt="call" />
+                <p>+7 (965) 237-44-49</p>
+              </div>
+              <select>
+                <option value="Ru">Русский</option>
+                <option value="KG">Кыргызча</option>
+                <option value="En">English</option>
+              </select>
+                <p>Личный кабинет</p>
+            </div>
+            </div>
         </div>
-      </div>
+        <div className="center-header container">
+  <img src={logo} alt="Логотип" className="logo" />
+  <div className="center-right">
+    <div className="search">
+      <input placeholder="Введите поисковой запрос.." type="text" />
+      <button className="btn-search">
+        <img src={search} alt="Поиск" />
+        Найти
+      </button>
+    </div>
+  </div>
+  <div className="cart-heart">
+    <div className="icon">
+      <img src={heart} alt="Избранное" />
+      <p>Избранное</p>
+    </div>
+    <div className="icon">
+      <img src={cart} alt="Корзина" />
+      <p>Моя корзина</p>
+    </div>
+  </div>
+        </div>
 
-      {/* Основной блок */}
-      <div className="header-main container">
-        <div className="header-main-top">
-          <img src={logo} alt="Логотип компании" />
-          
-          <div className="search">
-            <input type="text" placeholder="Введите поисковой запрос..." />
-            <button className="btn-search">
-              <img src={search} alt="Иконка поиска" />
-              Найти
-            </button>
+
+        <div className="bottom-header container">
+          <div className="selects">
+        <select>
+  <option value="" disabled selected>Apple</option>
+  <option value="iphone-xs">iPhone XS</option>
+  <option value="iphone-11-pro">iPhone 11 Pro</option>
+  <option value="iphone-12">iPhone 12</option>
+  <option value="iphone-13">iPhone 13</option>
+  <option value="iphone-14">iPhone 14</option>
+        </select>
+        <select>
+          <option value="" disabled selected>Huawei</option>
+
+          <option value="huawei-p30">Huawei P30</option>
+    <option value="huawei-p40">Huawei P40</option>
+    <option value="huawei-mate-30">Huawei Mate 30</option>
+    <option value="huawei-mate-40">Huawei Mate 40</option>
+        </select>
+          <select>
+          <option value="" disabled selected>Xiaomi</option>
+          <option value="xiaomi-mi-11">Xiaomi Mi 11</option>
+    <option value="xiaomi-redmi-note-10">Xiaomi Redmi Note 10</option>
+    <option value="xiaomi-poco-x3">Xiaomi Poco X3</option>
+    <option value="xiaomi-mi-10">Xiaomi Mi 10</option>
+          </select>
+        <select>
+        <option value="" disabled selected>Samsung</option>
+
+        <option value="galaxy-s20">Samsung Galaxy S20</option>
+    <option value="galaxy-s21">Samsung Galaxy S21</option>
+    <option value="galaxy-note-20">Samsung Galaxy Note 20</option>
+    <option value="galaxy-z-fold">Samsung Galaxy Z Fold</option>
+        </select>
           </div>
-
-            <div className="cart-heart">
-          <img src={heart} alt="Личный кабинет" />
-          <img src={cart} alt="Корзина" />
-            </div>
+          <div className="uls">
+            <ul>
+              <li>Питание и кабели</li>
+              <li>Powerbank</li>
+              <li>Акции</li>
+              <li>Прайс-лист</li>
+            </ul>
+          </div>
         </div>
       </div>
     </header>
