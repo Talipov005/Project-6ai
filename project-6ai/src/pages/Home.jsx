aa04bce0c03a269c926cd5465142de13d913166d
Talipov005 <talipovokuu@gmail.com>
2025-04-04 00:16:02 +0600

    banner

--- project-6ai/src/pages/Home.jsx
+++ project-6ai/src/pages/Home.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import "./Home.scss"
 import Banner from '../components/banner/Banner'
+import Popular from '../components/popular/Popular'
+import card1 from "../assets/svg/card 01.svg"
+import card2 from "../assets/svg/card 02.svg"
+import card3 from "../assets/svg/card 03.svg"
+import card4 from "../assets/svg/card 04.svg"
+
 
 function Home() {
   return (
     <div>
       <Banner/>
+      <div className='brend container'>
+        <h1>Выберите бренд</h1>
+        <div className='cards'>
+          <div>
+          <img src={card1} alt="" />
+          </div>
+          <div>
+          <img src={card2} alt="" />
+          </div>
+          <div>
+          <img src={card3} alt="" />
+        </div>
+        <div>
+          <img src={card4} alt="" />
+       </div>
+        </div>
+      </div>
+      <Popular/>
     </div>
   )
 }
