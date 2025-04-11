afee63f12d06feee20ba6ae0a22c3d7fb35cf7ed
Talipov005 <talipovokuu@gmail.com>
2025-04-11 17:44:50 +0600

    Model

--- project-6ai/src/router.jsx
+++ project-6ai/src/router.jsx
@@ -5,34 +5,38 @@ import Garant from "./pages/Garant";
 import Dostavka from "./pages/Dostavka";
 import Komponi from "./pages/Komponi";
 import Kontact from "./pages/Kontact";
-
+import Madel from "./pages/Madel";
 
 export const myRouter = createBrowserRouter([
-    {
-        path:"/",
-        element:<Layout/>,
-        children:[
-            {
-              path:"",
-              element:<Home/>
-            },
-            {
-              path:"garant",
-              element:<Garant/>
-            },
-            {
-              path:"dostavka",
-              element:<Dostavka/>
-            },
-            {
-              path:"komponi",
-              element:<Komponi/>
-            },
-            {
-              path:"kontact",
-              element:<Kontact/>
-            },
-
-           ]
-          }
-])
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "garant",
+        element: <Garant />,
+      },
+      {
+        path: "dostavka",
+        element: <Dostavka />,
+      },
+      {
+        path: "komponi",
+        element: <Komponi />,
+      },
+      {
+        path: "kontact",
+        element: <Kontact />,
+      },
+      {
+        path: "model/:brand",  // теперь будет передаваться бренд, а не id
+        element: <Madel />,
+      }
+      
+    ],
+  },
+]);
