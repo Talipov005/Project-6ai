8d609715b7227125ba317f3d4da017c413847276
Yryskeldi <Yrys>
2025-04-27 15:32:57 +0600

    Media zapros

--- project-6ai/src/pages/Komponi.jsx
+++ project-6ai/src/pages/Komponi.jsx
@@ -1,27 +1,44 @@
 import React from 'react';
-import "./Komponi.scss";
+import './Komponi.scss';
 import Card from '../components/besh-card/card';
 import AnimatedLayout from './Animation';
 
 function Komponi() {
   return (
     <AnimatedLayout>
-<div className="kompani-wrapper container">
-      <div className="kompani-wrapper__breadcrumb">Главная / О компании</div>
-      <h2 className="kompani-wrapper__title">О компании <br />No Doors Technology</h2>
+      <div className="kompani container">
+        <div className="kompani-breadcrumb">Главная / О компании</div>
+        <h2 className="kompani-title">
+          О компании <br /> No Doors Technology
+        </h2>
 
-      <div className="kompani-wrapper__content">
-        <div className="kompani-wrapper__text">
-          <p>Разнообразный и богатый опыт консультация с широким активом требуют определения и уточнения соответствующих условий активизации.</p>
-          <p>Разнообразный и богатый опыт постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет оценить значение системы обучения кадров, соответствует насущным потребностям.</p>
-          <p>Равным образом рамки и место обучения кадров представляет собой интересный эксперимент проверки соответствующих условий активизации.</p>
-          <p>Равным образом новая модель организационной деятельности требует определения и уточнения систем массового участия.</p>
+        <div className="kompani-content">
+          <div className="kompani-text">
+            <p>
+              Разнообразный и богатый опыт консультация с широким активом требуют
+              определения и уточнения соответствующих условий активизации.
+            </p>
+            <p>
+              Разнообразный и богатый опыт постоянное
+              информационно-пропагандистское обеспечение нашей деятельности
+              позволяет оценить значение системы обучения кадров, соответствует
+              насущным потребностям.
+            </p>
+            <p>
+              Равным образом рамки и место обучения кадров представляет собой
+              интересный эксперимент проверки соответствующих условий
+              активизации.
+            </p>
+            <p>
+              Равным образом новая модель организационной деятельности требует
+              определения и уточнения систем массового участия.
+            </p>
+          </div>
+
+          <div className="kompani-image" />
         </div>
-
-        <div className="kompani-wrapper__image" />
+        <Card />
       </div>
-      <Card/>
-    </div>
     </AnimatedLayout>
   );
 }
--- project-6ai/src/pages/Kontact.jsx
+++ project-6ai/src/pages/Kontact.jsx
@@ -9,26 +9,26 @@ import AnimatedLayout from "./Animation";
 function Kontact() {
   return (
     <AnimatedLayout>
-      <div className="kontact-container container">
-        <div className="kompani-wrapper__breadcrumb">Главная / О контакты</div>
+      <div className="kontact container">
+        <div className="kontact-breadcrumb">Главная / Контакты</div>
         <h1>Контакты</h1>
         <div className="kontact-info">
           <div className="kontact-details">
             <div className="kontact-item">
-              <img src={icon} alt="phone" />
+              <img src={icon} alt="Телефон" />
               <span>+996-995-56-56-32</span>
             </div>
             <div className="kontact-item">
-              <img src={icon2} alt="location" />
+              <img src={icon2} alt="Адрес" />
               <span>г. Бишкек, ул. Абдымомунова 109/1</span>
             </div>
             <div className="kontact-item">
-              <img src={icon3} alt="vk" />
+              <img src={icon3} alt="Социальные сети" />
               <span>vk.com/long_link_name</span>
               <span>instagram.com/long_ling_name</span>
             </div>
             <div className="kontact-item">
-              <img src={icon4} alt="info" />
+              <img src={icon4} alt="Информация о компании" />
               <span>
                 ООО "No Doors Technology"
                 <br />
@@ -41,18 +41,18 @@ function Kontact() {
             </div>
           </div>
           <div className="kontact-map">
-          <div className="map-frame">
-  <iframe
-    title="bishkek-philharmonic-map"
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2923.0080509826964!2d74.60009997658994!3d42.87714397115019!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389ec8b79b54e3e5%3A0xbec48e5bbf74cc02!2z0JHQsNC30LDRgNC60LjQuSDQv9C10YDQstC10YDQvdC40Lkg0KXQvdGC0L7QstCw0YDQug!5e0!3m2!1sru!2skg!4v1713690710831!5m2!1sru!2skg"
-    width="100%"
-    height="350"
-    style={{ border: 0 }}
-    allowFullScreen=""
-    loading="lazy"
-    referrerPolicy="no-referrer-when-downgrade"
-  ></iframe>
-</div>
+            <div className="map-frame">
+              <iframe
+                title="bishkek-philharmonic-map"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2923.0080509826964!2d74.60009997658994!3d42.87714397115019!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389ec8b79b54e3e5%3A0xbec48e5bbf74cc02!2z0JHQsNC30LDRgNC60LjQuSDQv9C10YDQstC10YDQvdC40Lkg0KXQvdGC0L7QstCw0YDQug!5e0!3m2!1sru!2skg!4v1713690710831!5m2!1sru!2skg"
+                width="100%"
+                height="350"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
           </div>
         </div>
       </div>
